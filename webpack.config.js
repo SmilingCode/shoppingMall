53c53010b921ecfe953f7281063da63bbf378b21
SmilingCode <ericc_j@icloud.com>
2019-08-22 17:25:55 +1000

    domain

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,13 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+let WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
+
 module.exports = {
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/',
+        publicPath: WEBPACK_ENV === 'dev' ? '/dist/' : '//hidden-oasis-31543.herokuapp.com/dist/',
         filename: 'js/app.js'
     },
     resolve: {
