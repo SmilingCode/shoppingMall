6f86e6e6c81831a24a839fbfe6a55d811287fc88
SmilingCode <ericc_j@icloud.com>
2019-09-24 16:35:16 +1000

    heroku config

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const proxy = require('http-proxy-middleware');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(
+    '/manage/*',
+    proxy({
+        target: 'http://admintest.happymmall.com/',
+        changeOrigin: true
+    })
+)
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
