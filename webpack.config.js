631b8e4c2d02143c736998e33289e41e985f00d6
SmilingCode <ericc_j@icloud.com>
2019-09-24 17:16:45 +1000

    path problem

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/',
+        publicPath: '/',
         filename: 'bundle.js'
     },
     resolve: {
